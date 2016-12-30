4690c9b040b4741f7edcc68ee625c0adbaa00823
deepikathakur05 <deepika.77thakur@gmail.com>
2016-12-30 12:38:29 +0530

    Add files via upload
    example about scoping

---
+++ index.js
@@ -0,0 +1,16 @@
+var  a = 5;
+console.log(a)
+var  b = 6;
+console.log (b)
+function add()
+{
+	var a = 3;
+	var b = 8;
+	var c = a+b;
+
+	console.log (c)
+}
+add ();
+console.log(a)
+console.log(b)
+console.log(c)
